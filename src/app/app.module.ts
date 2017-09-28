32f382256ec6739b854f6fb725b8b70e8c41704c
Foad R ShariatPanahi <jesuschrist.love@hotmail.com>
2017-09-28 16:04:41 -0700

    Foad - Add basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { EditUserComponent } from './users/edit-user/edit-user.component';
 import { AccountsComponent } from './accounts/accounts.component';
+
+const routes: Routes = [
+  { path:'', component: HomeComponent },
+  { path:'users', component: UsersComponent },
+  { path:'accounts', component: AccountsComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +23,8 @@ import { AccountsComponent } from './accounts/accounts.component';
     AccountsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
 
+  onClick(){
+    this.router.navigate(['users'], {relativeTo: this.route});
+  }
 }
