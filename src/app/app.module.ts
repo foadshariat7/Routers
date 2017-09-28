6befeba55c8a13dea988afde0bafa284bd6c8445
Foad R ShariatPanahi <jesuschrist.love@hotmail.com>
2017-09-28 15:08:03 -0700

    Foad - Generate some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { EditUserComponent } from './users/edit-user/edit-user.component';
+import { AccountsComponent } from './accounts/accounts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    EditUserComponent,
+    AccountsComponent
   ],
   imports: [
     BrowserModule
